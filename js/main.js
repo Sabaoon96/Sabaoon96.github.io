b9623d312785c45ee4a669c5dc658bb0827e9f5b
Sabaoon Raza Khan <Sabaoon96@users.noreply.github.com>
2019-03-31 19:44:23 +1300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -56,7 +56,7 @@ $(window).load(function(){
 
 
 $('#countdown').countdown({
-	date: "January 30, 2019 12:00:00",
+	date: "August 30, 2019 12:00:00",
 	render: function(data) {
 	  var el = $(this.el);
 	  el.empty()
